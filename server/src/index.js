048567226fd1e57f0851897830695308609a4b8e
romandronov <dronov.r.a@edu.mirea.ru>
2020-01-07 16:24:03 +0300

    f

--- server/src/index.js
+++ server/src/index.js
@@ -7,13 +7,13 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 
-const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
+//const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
 
 // App
 const app = express();
 
 // Static files
-app.use(express.static(CLIENT_BUILD_PATH));
+//app.use(express.static(CLIENT_BUILD_PATH));
 
 // API
 app.get('/api', (req, res) => {
@@ -25,9 +25,9 @@ app.get('/api', (req, res) => {
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
+/*app.get('*', function(request, response) {
   response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
-});
+});*/
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
